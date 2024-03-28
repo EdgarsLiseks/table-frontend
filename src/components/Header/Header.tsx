5918f85fa5dbafd625f7da97caa2ae7d7cd01905
EdgarsLiseks <edgars.liseks@gmail.com>
2024-03-28 06:00:37 +0200

    Remove logo & tr style

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,17 +2,9 @@ import Navigation from "../Navigation/Navigation"
 
 const Header = () => {
   return (
-    <>
-      <header className="header">
-        <div className="logo">
-          <img src="vite.svg" alt="React app logo" />
-        </div>
-
-        <div className="nav">
-          <Navigation />
-        </div>
-      </header>
-    </>
+    <header className="header">
+      <Navigation />
+    </header>
   )
 }
 export default Header
